db2fb5f90efc3bd561461df8b034c8a63f90294d
shristov <shristov@callflowlab.com>
2024-06-03 16:44:57 +0300

    setup

---
+++ epic-games-hub/src/main/java/com/epicgameshub/epic_games_hub/EpicGamesHubApplication.java
@@ -0,0 +1,13 @@
+package com.epicgameshub.epic_games_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicGamesHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicGamesHubApplication.class, args);
+	}
+
+}
---
+++ epic-games-hub/src/test/java/com/epicgameshub/epic_games_hub/EpicGamesHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicgameshub.epic_games_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicGamesHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
